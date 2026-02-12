6dd59eb5f9d4708472cefc6fc4134fc7be24fac4
Joesynth <133770468+Joesynth@users.noreply.github.com>
2026-02-12 12:54:54 -0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,21 @@
+
+
+
+				/*SLIDESHOW*/
+				
+var images = ["m1.jpg","m2.jpg","m3.jpg"];
+var imageNumber = 0;
+var imageLength = images.length -1;
+//alert(images[imageNumber]);   // exibe o conteúdo dum array somente
+function changeImage(x) {
+	imageNumber += x;
+	if (imageNumber > imageLength) {
+		imageNumber = 0;
+	}
+	document.getElementById("slideshow").src = images[imageNumber];
+	return false;
+}
+function autoRun () {				
+	setInterval("changeImage(1)",4000);
+}
+window.addEventListener("load",autoRun);
